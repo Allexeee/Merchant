b028346c2e4e60a681bafe60f4d1b5e4f1fbfa52
Allexeee <aleksey_ohezin@mail.ru>
2020-01-15 23:01:25 +0200

    Update

--- Merchant.nim
+++ Merchant.nim
@@ -1,45 +1,64 @@
-import Db
+import Db, terminal
 
 var money = 100
 
+eraseScreen(stdin)
+setCursorPos(stdin, 0, 0)
 echo "Привет, торговец, как тебя зовут?"
 let name: string = readLine(stdin)
-echo "Отлично! Итак, ", name, ", ты должен покупать товары у одних клиентов и продавать их другим."
-echo "Это просто!"
-echo "Но смотри - не останься без денег и товара"
-echo "Вот тебе склад туда залезут любые 10 предметов"
-echo "Ну а денег у тебя, думаю найдется"
-echo "(У Вас ", money, " золотых)"
+hideCursor()
 
-echo ""
+echo """Отлично! Итак, """, name,
+    """, ты должен покупать товары у одних клиентов и продавать их другим.
+  Это просто!
+  Но смотри - не останься без денег и товара
+  Вот тебе склад туда залезут любые 10 предметов
+  Ну а денег у тебя, думаю найдется
+  (У Вас """, money, """ золотых)
+  """
 
+echo "Нажмите на любую клавишу, чтобы продолжить"
+
+discard getch()
+
+var i: int = 0
 while true:
+  # discard getch()
+  eraseScreen(stdin)
+  setCursorPos(stdin, 0, 0)
+
   var (classType, want, itemType) = getRandomClient()
 
-  echo "============================="
-  echo "У Вас новый клиент!"
+  echo "У Вас новый клиент! (", i, ")"
   echo "Класс: ", classType.name
   case want
-    of Want.Buy: echo "Покупает: ", itemType
-    of Want.Sell: echo "Продает: ", itemType
-  echo "По цене: ", 10
+    of Want.Buy: echo "Покупает: ", itemType, " по цене 12"
+    of Want.Sell: echo "Продает: ", itemType, " по цене 10 (можно продать за 12)"
   echo "============================="
+  echo "У вас ", money, " монет"
 
   echo ""
   case want
     of Want.Buy: echo "[X] Продать"
     of Want.Sell:
-      if(money-10>0): echo "[X] Купить"
-  
+      if(money-10 > 0): echo "[X] Купить"
+
   echo "[C] Отказаться"
 
-  var input = readChar(stdin)
-  if input == 'X' or input == 'x':
-    if want == Want.Buy and money-10>0:
-      money += 10
-    else:
-      money -= 10
-    echo "Монет: ", money
+  while true:
+    var input = getch()
+    case input:
+      of 'X', 'x':
+        if want == Want.Sell:
+          if money-10 > 0:
+            money -= 10
+        else:
+          money += 12
+        # echo "Монет: ", money
+        break
+      of 'C', 'c':
+        break
+      else:
+        continue
 
-  input = readChar(stdin)
-  
+  inc(i)
