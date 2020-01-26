5b071f0e88c45a351e377f53e30436b9110aaf6b
Allexeee <aleksey_ohezin@mail.ru>
2020-01-26 21:51:25 +0200

    v.0.4
    Небольшие правки в выводимой информации о товаре.
    Добавлен экран после сделки

--- Merchant.nim
+++ Merchant.nim
@@ -18,6 +18,84 @@ var i: int = 0
 while endGame == End.None:
   eraseScreen(stdin)
   setCursorPos(stdin, 0, 0)
+    
+  var (classType, want, item) = getRandomClient()
+
+  echo "У Вас новый клиент! (Ход: ", i, ")"
+  echo "Класс: ", classType.name
+  case want
+    of Want.Buy: echo "Покупает: ", item.name
+    of Want.Sell: echo "Продает: ", item.name
+  echo "============================="
+  echo ""
+  echo "У вас ", money, " монет"
+
+  let itemIndex = findInInvetory(item)
+  let costSell = item.cost + (0.1 * item.cost.toFloat).toInt
+
+  case want
+    of Want.Buy: 
+      if itemIndex != -1:
+        echo "[X] Продать ",item.name," за ", costSell
+      else:
+        echo "У вас нет предмета ", item.name
+    of Want.Sell:
+      if(money-item.cost > 0): 
+        if inventory.len < 10:
+          echo "[X] Купить ",item.name," за ", item.cost
+        else: echo "Инвентарь заполнен"
+      else: echo "Не хватает монет"
+
+  echo "[C] Отказаться"
+
+  echo ""
+  showInventory()
+
+  var isSelled, isBuyed = false
+
+  while true:
+    var input:char
+    try:
+      input = getch()
+    except:
+      echo "Используйте другую раскладку клавиатуры"
+    case input:
+      of 'X', 'x':
+        if want == Want.Sell:
+          if money - item.cost > 0 and inventory.len < 10:
+            money -= item.cost
+            isBuyed = true
+            # if money <= 0 : endGame = End.Lose
+            inventory.add(item)
+          else:continue
+        else:
+          if itemIndex != -1:
+            money += costSell
+            isSelled = true
+            if money >= 200 : endGame = End.Win
+            inventory.del(itemIndex)
+          else:continue
+        # echo "Монет: ", money
+        break
+      of 'C', 'c':
+        break
+      else:
+        continue
+
+  eraseScreen(stdin)
+  setCursorPos(stdin, 0, 0)
+  
+  inc(i)
+
+  if isBuyed or isSelled:
+    if isSelled:
+      echo "Вы продали ", item.name, " за ", costSell, " монет"
+
+    elif isBuyed:
+      echo "Вы купили ", item.name, " за ", item.cost, " монет"
+
+    echo "Итого монет: ", money
+    echo ""
 
   if(i mod 10) == 0 and i != 0:
     money -= 20
@@ -37,67 +115,15 @@ while endGame == End.None:
 
     echo "Цена предмета \"", item.name,"\" изменилась на ", costIncrease 
     echo ""
-    
-  var (classType, want, item) = getRandomClient()
 
-  echo "У Вас новый клиент! (", i, ")"
-  echo "Класс: ", classType.name
-  case want
-    of Want.Buy: echo "Покупает: ", item.name
-    of Want.Sell: echo "Продает: ", item.name
-  echo "============================="
-  echo ""
-  echo "У вас ", money, " монет"
-
-  let itemIndex = findInInvetory(item)
-  let costSell = item.cost + (0.1 * item.cost.toFloat).toInt
-
-  case want
-    of Want.Buy: 
-      if itemIndex != -1:
-        echo "[X] Продать ",item.name," за ", costSell
-      else:
-        echo "У вас нет этого предмета"
-    of Want.Sell:
-      if(money-item.cost > 0): 
-        if inventory.len < 10:
-          echo "[X] Купить ",item.name," за ", item.cost
-        else: echo "Инвентарь заполнен"
-      else: echo "Не хватает монет"
-
-  echo "[C] Отказаться"
-
-  echo ""
-  showInventory()
-
-  while true:
-    var input:char
+    echo "Нажмите любую клавишу, чтобы продолжить"
     try:
-      input = getch()
+      discard getch()
     except:
       echo "Используйте другую раскладку клавиатуры"
-    case input:
-      of 'X', 'x':
-        if want == Want.Sell:
-          if money - item.cost > 0 and inventory.len < 10:
-            money -= item.cost
-            if money <= 0 : endGame = End.Lose
-            inventory.add(item)
-          else:continue
-        else:
-          if itemIndex != -1:
-            money += costSell
-            if money >= 200 : endGame = End.Win
-            inventory.del(itemIndex)
-          else:continue
-        # echo "Монет: ", money
-        break
-      of 'C', 'c':
-        break
-      else:
-        continue
 
-  inc(i)
+
+
 
 eraseScreen(stdin)
 setCursorPos(stdin, 0, 0)
